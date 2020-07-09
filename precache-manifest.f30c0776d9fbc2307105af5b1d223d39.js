eddb4c9beefa2a1087eff45d574ed9f731e32525
Eduardo Yutaka Nakanishi <eduardo.nakanishi@gmail.com>
2020-07-09 12:53:09 -0300

    Updates

---
+++ precache-manifest.f30c0776d9fbc2307105af5b1d223d39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a16bb6d0c82678f69dcc0d811ca11c15",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "980e03fb6b409ecdceb4",
+    "url": "/calculator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c19f4fc6d3a73105c228",
+    "url": "/calculator/static/js/2.7765b133.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/calculator/static/js/2.7765b133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "980e03fb6b409ecdceb4",
+    "url": "/calculator/static/js/main.8ee8b3e9.chunk.js"
+  },
+  {
+    "revision": "ee4a148f714da062db4f",
+    "url": "/calculator/static/js/runtime-main.f0d0de70.js"
+  }
+]);
